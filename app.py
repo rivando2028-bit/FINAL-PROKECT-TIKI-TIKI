615918ec1d40a227c3b1898d6ed246ce76a576e3
rivando2028-bit <rivando.2028@olifanthighschool.sch.id>
2026-05-15 15:00:13 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
-# INPUT Saldo_awal
-saldo_awal = float(input("Masukkan Saldo Awal: "))
+import streamlit as st
 
-# IF Saldo_awal > 0 THEN
-if saldo_awal > 0:
-    sisa = saldo_awal
-    log_pengeluaran = []
+st.title("Program Pengeluaran Saldo")
 
-    # WHILE Sisa > 0 DO
-    while sisa > 0:
-        pengeluaran = float(input("Masukkan Pengeluaran: "))
-        
-        # IF Pengeluaran >= 2000 THEN
-        if pengeluaran >= 2000:
-            sisa = sisa - pengeluaran
-            log_pengeluaran.append(pengeluaran) # TAMBAHKAN ke Log
-        
-        # OUTPUT Sisa, Log_Pengeluaran
-        print("Sisa Saldo:", sisa)
-        print("Catatan Pengeluaran:", log_pengeluaran)
+# INPUT SALDO AWAL
+saldo_awal = st.number_input("Masukkan saldo awal", min_value=0, step=1000)
 
-print("Program Selesai.")
+# Tombol mulai
+if st.button("Mulai"):
+
+    # IF SALDO > 0
+    if saldo_awal > 0:
+
+        sisa = saldo_awal
+        log_pengeluaran = []
+
+        # WHILE
+        while sisa > 0:
+
+            pengeluaran = st.number_input(
+                f"Masukkan pengeluaran (Sisa saldo: {sisa})",
+                min_value=0,
+                step=1000,
+                key=sisa
+            )
+
+            if st.button(f"Tambah Pengeluaran {sisa}"):
+
+                # IF PENGELUARAN VALID
+                if pengeluaran >= 2000 and pengeluaran <= sisa:
+
+                    sisa = sisa - pengeluaran
+                    log_pengeluaran.append(pengeluaran)
+
+                    st.success(f"Pengeluaran berhasil ditambahkan: {pengeluaran}")
+                    st.write("Sisa saldo:", sisa)
+                    st.write("Log Pengeluaran:", log_pengeluaran)
+
+                else:
+                    st.error("Pengeluaran tidak valid")
+
+                # STOP WHILE JIKA SALDO HABIS
+                if sisa == 0:
+                    st.warning("Saldo habis")
+
+                break
+
+    else:
+        st.error("Saldo awal harus lebih dari 0")
